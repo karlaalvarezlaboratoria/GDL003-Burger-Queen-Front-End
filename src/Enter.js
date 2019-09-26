5572d846a09ae9101a542d710b9d4fa32d6d4e35
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-09-26 16:32:58 -0500

    Primera iteraciÃn creaciÃn de componentes Se crea componente de patalla de inicio Se agregan logos a carpeta src Se crea componente CreateComand

---
+++ src/Enter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './Ne-koffee Aqua oscuro.png';
+import './App.css';
+
+function Enter() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <input className='Input' type= 'text' >
+        </input>
+        <button className= "Button">
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default Enter;
