e86449d6fd78bc2ce9ee0bd47d79cf1a541f9643
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2019-09-27 15:22:10 -0500

    Se modifica componente "MenuItems" y su css -El componente trae la informaciÃn del back end, y pinta botones con el nombre de los productos.

--- src/Components/MenuItems.jsx
+++ src/Components/MenuItems.jsx
@@ -5,19 +5,51 @@ class RenderCommand extends Component {
     constructor(){
         super();
         this.state = {
-            menu: [
-                { name:'Café', price:15 },
-                { name:'Té', price:10 }
-            ]
+            menu: []
         }
     }
+
+    
+    componentDidMount(){ 
+        fetch('https://gdl003-burger-queen-back-end.engkarinacabrera.now.sh/products')
+                .then(data => data.json())
+                .then(data => {
+                    let arr= [];
+                    let keac= () =>{
+                        data.products.forEach((element) => {
+                           arr.push(element)                        
+                      }); console.log(arr)                   
+                        return arr;
+                      }
+                      keac()
+
+                    this.setState({
+                        menu: arr 
+                          })
+                      })
+                    }  
+                    
+                     
+         
+
+
     render() {
+        const command = this.state;
+        
+        console.log(command)
+        
+
         return (
             <div className="menuItems">
-            <p>Aqui se imprimen los elementos del menu</p>
-            <p>{this.state.menu.name}</p>
+              <p>Aquí van los botones de lo que hay en mi menú</p>
+              {command.menu.map((command, key)=>(
+                <button key={key} className= 'button'>{command.name}</button> 
+              ))
+              } 
             </div>
+            
+        
         )
-    }
+}
 }
 export default RenderCommand;
--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 import {ProtectedRoute} from './ProtectedRoute'
-import Enter from './Enter';
-import CreateCommand from './CreateCommand'
+import Enter from './Components/Enter';
+import Create from './Components/CreateCommand'
 
 const Router = () => (
 
 
   <BrowserRouter>
     <Switch>
-      <ProtectedRoute path= "/create/" component = {CreateCommand}/> 
+      <ProtectedRoute path= "/create/" component = {Create}/> 
       <Route exact path = "/" component ={Enter}/>
       <Route path = '*' component = {() => '404 NOT FOUND'}/> 
     </Switch>
