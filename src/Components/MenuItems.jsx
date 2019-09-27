8c4e3431472d8f4683e03c8a7e647e723f8cbda4
Gloria <izayura@gmail.com>
2019-09-27 15:21:16 -0500

    Avances en componente RenderCommand -Se creó función para imprimir una tabla con los datos de un array provisional en RenderCommand -Se modificó RenderCommand.css para dar estilo a la tabla -Se comenzó a trabajar en MenuItems para traer la información creada ahí y poder iterar directamente esos datos en lugar del array provisional -Se corrigó detalle en Router.js para poder visualizar correctamente los componentes Enter y CreateCommand

--- src/Components/MenuItems.jsx
+++ src/Components/MenuItems.jsx
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
 import './MenuItems.css';
 
+/*
 class RenderCommand extends Component {
-    constructor(){
-        super();
+    constructor(props) {
+        super(props);
+        this.addProduct = this.addProduct.bind(this);
         this.state = {
-            menu: [
-                { name:'Café', price:15 },
-                { name:'Té', price:10 }
+            menuList: [
+                { name: 'Café', price: 15 },
+                { name: 'Té', price: 10 },
+                { name: 'Milkshake', price: 25 }
             ]
         }
     }
+
+    addProduct() {
+        console.log("Hola Glo :D");
+        console.log(this.menuId)
+        console.log(this.menuId.button);
+    }
+
     render() {
+        let { menuList } = this.state
         return (
             <div className="menuItems">
-            <p>Aqui se imprimen los elementos del menu</p>
-            <p>{this.state.menu.name}</p>
+                <p>Aqui se imprimen los elementos del menu</p>
+                <ul>
+                    {menuList.map(element => (
+                            <div key={element.name} id={element.name} ref={button => { this.menuId = button; }}>
+                                <p>{element.name}</p>
+                                <button value={element.name} onClick={this.addProduct}>{element.name}</button>
+                            </div>
+                        ))  
+                    }
+                </ul>
+
             </div>
         )
     }
-}
+
+} 
+*/
+
 export default RenderCommand;
--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 import {ProtectedRoute} from './ProtectedRoute'
-import Enter from './Enter';
-import CreateCommand from './CreateCommand'
+import Enter from './Components/Enter';
+import CreateCommand from './Components/CreateCommand'
 
 const Router = () => (
 
