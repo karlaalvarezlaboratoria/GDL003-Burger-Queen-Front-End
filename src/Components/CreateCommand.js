3a1c5582148562f40b818b14eeebb1a8d768c4d6
Gloria <izayura@gmail.com>
2019-10-02 13:33:06 -0500

    Se dividió la lógica entre componente padre e hijos -Se llevó ComponentDidMount a CreateCommand y de ahí se llevan los valores del array a MenuItems para imprimir los botones -Se comentó parte de ComponentDidMount en la espera de poder volver hacer fetch con BackEnd

--- src/Components/CreateCommand.js
+++ src/Components/CreateCommand.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CreateCommand.css';
 import RenderCommand from './RenderCommand';
-
+import MenuItems from './MenuItems';
 import Cancel from './CancelComand';
 
 
-function Create() {
-  return (
-    <div> 
-    <Cancel/>
-    <p>Este va a se mi pantalla de comandas</p>
-    <RenderCommand /*items={this.state.orderItems}*//>
-    <MenuItems /*addItem={this.addItemHandler}*//>
-    </div>
-   );
+class Create extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menu: []
+    }
+  }
+  componentDidMount() {
+    // fetch('https://gdl003-burger-queen-back-end.engkarinacabrera.now.sh/products')
+    //     .then(data => data.json())
+    //     .then(data => {
+    //         let arr = [];
+    //         let keac = () => {
+    //             data.products.forEach((element) => {
+    //                 arr.push(element)
+    //             }); console.log(arr)
+    //             return arr;
+    //         }
+    //         keac()
+
+    this.setState({
+      menu: [
+        { id: 1, name: 'Café', price: 15 },
+        { id: 2, name: 'Té', price: 10 },
+        { id: 3, name: 'Chocolate', price: 20 }]
+      //             menu: arr
+    })
+    //     })
+  }
+
+  render() {
+    const command = this.state;
+    console.log(command);
+    return (
+      <div>
+        <Cancel />
+        <p>Este va a se mi pantalla de comandas</p>
+        <RenderCommand /*items={this.state.orderItems}*/ />
+        <MenuItems menu={this.state.menu} />
+
+      </div>
+    );
+  }
 }
 
 export default Create;
