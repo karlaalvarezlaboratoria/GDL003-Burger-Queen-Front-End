16f658f40a033322928e50491493a512311b0550
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2020-01-17 16:29:30 -0600

    Add user management button

--- src/Components/ChooseUser.js
+++ src/Components/ChooseUser.js
@@ -3,25 +3,22 @@ import './Enter.css';
 import { withRouter } from 'react-router-dom';
 
 class User extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className='titleUser'>Select your user</h1>
-                <button
-                    className='buttonUser'
-                    onClick={() => this.props.history.push('/select')}
-                >
-                    Waiter
+  render() {
+    return (
+      <div>
+        <h1 className="titleUser">Select your user</h1>
+        <button className="buttonUser" onClick={() => this.props.history.push('/management')}>
+          Manager
         </button>
-                <button
-                    className='buttonUser'
-                    onClick={() => this.props.history.push('/kitchen')}
-                >
-                    Coffee Bar
+        <button className="buttonUser" onClick={() => this.props.history.push('/select')}>
+          Waiter
         </button>
-            </div>
-        );
-    }
+        <button className="buttonUser" onClick={() => this.props.history.push('/kitchen')}>
+          Coffee Bar
+        </button>
+      </div>
+    );
+  }
 }
 
 export default withRouter(User);
