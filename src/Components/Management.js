300365a872dd3a5c022421131080ebe9c03e4c66
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2020-01-18 17:44:39 -0600

    Prueba con de cors con deploy

--- src/Components/Management.js
+++ src/Components/Management.js
@@ -9,6 +9,8 @@ class Manager extends Component {
     this.state = {
       menu: [],
       isOpen: false,
+      newProduct: {name:"", price:""},
+      // newMenu: {}
     };
   }
 
@@ -29,8 +31,48 @@ class Manager extends Component {
       );
   }
 
+
+//   saveNewProduct=()=>{
+//     this.setState({
+//       newProduct:{name: this.newName.value, price: this.newPrice.value}
+//   })
+// }
+
+  addNewProduct = (price, name, _id) => {
+    let newPrice= this.newPrice.value;
+    let newName = this.newName.value;
+    this.setState({
+      menu: [...this.state.menu, {price: newPrice, name:newName}]
+    })
+  }
+  
+  addProduct=()=>{
+    this.addNewProduct();
+    let deploy = 'https://app-nekoffee.herokuapp.com/api/products'
+    let tokenLocal = localStorage.getItem('Token');
+    let tokenHeader = 'Bearer ' + tokenLocal
+    let databody = this.state.menu;
+    console.log(JSON.stringify({ products: databody }))
+    fetch(deploy, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'authorization': tokenHeader
+      },
+      method: 'PUSH',
+      body: JSON.stringify({ products: databody })
+    }).then(res => res.json())
+      .then(data => console.log(data), this.setState({
+        isOpen: false,
+      }),)
+      .catch(err => console.log(err.message));
+  }
+
   render() {
+    // console.log(this.state.newProduct);
+    // console.log(name);
     let menu = this.state.menu;
+    console.log(...menu);
     return (
       <div>
         <header>
@@ -48,13 +90,13 @@ class Manager extends Component {
         <Dialog isOpen={this.state.isOpen} onClose={() => this.setState({ isOpen: false })}>
           <form>
             <label>
-              <input className="dialog-input" type="text" placeholder="New product"></input>
+              <input ref={input => { this.newName = input; }} className="dialog-input" type="text" placeholder="New product" ></input>
             </label>
             <label>
-              <input className="dialog-input" type="text" placeholder= "Price"></input>
+              <input ref={input => { this.newPrice = input; }} className="dialog-input" type="text" placeholder= "Price" ></input>
             </label>
           </form>
-          <button>Add</button>
+          <button onClick={()=> this.addProduct()}>Add</button>
           <button onClick={() => this.setState({ isOpen: false })}>Cancel</button>
         </Dialog>
 
