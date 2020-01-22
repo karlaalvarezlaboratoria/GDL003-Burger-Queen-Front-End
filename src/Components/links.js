078776fd89599cc37ba5575453a5f4971ddbc2de
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2020-01-22 10:34:39 -0600

    Archivo de prueba, contiene las variables de los links en caso de que haya que cambiar a modo local

---
+++ src/Components/links.js
@@ -0,0 +1,18 @@
+let products = 'https://app-nekoffee.herokuapp.com/api/products',
+//productsId= 'https://app-nekoffee.herokuapp.com/api/products/' + _id,
+orders ='https://app-nekoffee.herokuapp.com/api/orders' ,
+//ordersId= 'https://app-nekoffee.herokuapp.com/api/orders/' + _id ,
+
+localProducts = "http://localhost:3000//api/products",
+//localProductsId= "localhost:3000//api/products/" + _id ,
+localOrders= "http://localhost:3000//api/orders" ,
+//locaOrdersId= "localhost:3000//api/orders/" + _id ,
+
+password = 'https://app-nekoffee.herokuapp.com/api/authenticate',
+passwordLocal = 'http://localhost:3000/api/authenticate',
+
+ordersLink= localOrders,
+productsLink= localProducts,
+passwordLink= passwordLocal;
+
+export default (ordersLink, passwordLink, productsLink)
