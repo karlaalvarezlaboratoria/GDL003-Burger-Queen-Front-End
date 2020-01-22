a41968f6633f9a9c5b1b21a4ad4e1a53aeb94f49
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2020-01-22 10:33:44 -0600

    se crea función aparte para disminuir código en los componentes funciona con POST Y DELETE, (no se ha probado en PUT, en GET no funciona igual)

---
+++ src/Components/httpRequest.js
@@ -0,0 +1,25 @@
+//ESTA ES LA COPIA DEL POST
+
+const httpCall = (link, verb, body, other) => {
+    let deploy = link,
+      tokenLocal = localStorage.getItem("Token"),
+      tokenHeader = "Bearer " + tokenLocal,
+      databody = body
+    fetch(deploy, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        authorization: tokenHeader
+      },
+      method: verb,
+      body: JSON.stringify(databody)
+    })
+      .then(res => res.json())
+    //   .then(data =>
+    //     other()
+    //   )
+      .catch(err => console.log(err.message));
+  };
+  
+  export default httpCall;
+  
