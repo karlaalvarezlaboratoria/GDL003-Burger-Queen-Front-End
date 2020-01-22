6bde18f8ff170194dae5c8c33e57bbea60c7e77e
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2020-01-22 00:09:35 -0600

    Se corrige y quita código innecesario

--- src/Components/Enter.js
+++ src/Components/Enter.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './Ne-koffee Aqua oscuro.png';
 import './Enter.css';
 import { withRouter } from 'react-router-dom';
-import User from './ChooseUser';
 
 class Enter extends Component {
   constructor() {
@@ -10,82 +9,79 @@ class Enter extends Component {
     this.getToken = this.getToken.bind(this);
     this.state = {
       token: '',
-      access: false,
-      defaultToken: 'neko'
-    }
+      defaultToken: 'neko',
+    };
   }
 
   getToken() {
     let user = 'Nekoffee';
-    console.log(user)
+    console.log(user);
     let password = this.password.value;
-    console.log(password)
-    var sendPassword = 'https://app-nekoffee.herokuapp.com/api/authenticate'
+    console.log(password);
+    var sendPassword = 'https://app-nekoffee.herokuapp.com/api/authenticate';
     fetch(sendPassword, {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
       method: 'POST',
-      body: JSON.stringify({ name: user, password: password })
+      body: JSON.stringify({ name: user, password: password }),
     })
       .then(response => {
         return response.json();
-      }).then(data => {
-        console.log(data)
+      })
+      .then(data => {
+        console.log(data);
         if (data.success === true) {
-          this.setState({
-            access: true
-          })
+          this.props.history.push('/user');
         } else {
           this.setState({
-            errorPassword: true
-          })
+            errorPassword: true,
+          });
           this.password.value = '';
           return data.message;
         }
         const token = data.token;
-        console.log(data.token)
+        console.log(data.token);
         this.setState({
-          token: token
-        })
-        return token
-      }).then(token => {
+          token: token,
+        });
+        return token;
+      })
+      .then(token => {
         localStorage.setItem('Token', token);
         let tokenLocal = localStorage.getItem('Token');
         return console.log(tokenLocal);
-      }).catch(error => {
-        console.log(error)
       })
-    console.log(this.state.token)
+      .catch(error => {
+        console.log(error);
+      });
+    console.log(this.state.token);
   }
 
   render() {
     return (
-      this.state.access
-        ? (
-          <div className='App'>
-            <header className='App-header'>
-              <img src={logo} className='NekoffeeLogo' alt='logo' />
-              <User />
-            </header>
-          </div>
-        )
-        : (
-          <div className='App'>
-            <header className='App-header'>
-              <img src={logo} className='NekoffeeLogo' alt='logo' />
-              <form className='loginForm' method='post'>
-                <h1 className='titleUser'>Password</h1>
-                <p className={'messagePasswordIncorrect' + (this.state.errorPassword ? "" : " invisible")}>The password is incorrect</p> 
-                <input className='Password' type='password' value={this.state.defaultToken} ref={input => { this.password = input; }} />
-                <button type='button' onClick={this.getToken}>
-                  GET TOKEN
-                </button>
-              </form>
-            </header>
-          </div>
-        )
+      <header className="App-header">
+        <img src={logo} className="NekoffeeLogo" alt="logo" />
+        <form className="loginForm" method="post">
+          <h1 className="titleUser">Password</h1>
+          <p
+            className={'messagePasswordIncorrect' + (this.state.errorPassword ? '' : ' invisible')}>
+            The password is incorrect
+          </p>
+          <input
+            className="Password"
+            type="password"
+            value={this.state.defaultToken}
+            ref={input => {
+              this.password = input;
+            }}
+          />
+          <button type="button" onClick={this.getToken}>
+            GET TOKEN
+          </button>
+        </form>
+      </header>
     );
   }
 }
